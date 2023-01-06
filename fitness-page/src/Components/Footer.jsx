8cffd4087dc27ff40875c69d2205d79f3c34803f
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2023-01-06 10:48:51 +0000

    web addresses

--- fitness-page/src/Components/Footer.jsx
+++ fitness-page/src/Components/Footer.jsx
@@ -7,15 +7,15 @@ export default function Footer(){
         <a href="mailto:vaishalimehmi@yahoo.co.uk" class="footer__link">vaishalimehmi@yahoo.co.uk</a>
         <ul class="social-list">
             <li class="social-list__item">
-                <a class="social-list__link" href="https://uxfolio.com">
+                <a class="social-list__link" href="https://github.com/vm799?tab=repositories">
                     <i class="fab fa-github"></i>
                 </a></li>
                 <li class="social-list__item">
-                    <a class="social-list__link" href="https://uxfolio.com">
+                    <a class="social-list__link" href="https://www.linkedin.com/in/vaishali7/">
                         <i class="fab fa-linkedin"></i>
                     </a></li>
                     <li class="social-list__item">
-                        <a class="social-list__link" href="https://uxfolio.com">
+                        <a class="social-list__link" href="https://www.instagram.com/">
                             <i class="fab fa-instagram"></i>
                         </a></li>
         </ul>
